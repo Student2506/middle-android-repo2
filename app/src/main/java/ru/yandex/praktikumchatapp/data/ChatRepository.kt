ba35f87eb3ea94516083dcface9952e5e2f434b4
Захаров Павел <pzakharov83@gmail.com>
2025-01-15 17:56:16 +0300

    add "getReplyMessage should return a non-empty string" test

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
@@ -1,12 +1,27 @@
 package ru.yandex.praktikumchatapp.data
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retry
 
 class ChatRepository(
     private val api: ChatApi = ChatApi(),
 ) {
 
     fun getReplyMessage(): Flow<String> {
-        return api.getReply()
+        return api.getReply().retry { e ->
+            var currentDelay = 1000L
+
+            if (e is Exception) {
+                delay(currentDelay)
+                currentDelay *= DELAY_FACTOR
+                true
+            } else {
+                false
+            }
+        }
+    }
+    private companion object {
+        const val DELAY_FACTOR = 2
     }
 }
