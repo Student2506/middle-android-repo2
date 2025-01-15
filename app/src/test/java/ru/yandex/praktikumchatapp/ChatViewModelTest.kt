e4c04d0208ff4c1655f35c8b55300ea195b90573
Захаров Павел <pzakharov83@gmail.com>
2025-01-15 15:16:12 +0300

    complete testReceiveMessage_concurrentMessages

--- app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
+++ app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
@@ -1,7 +1,12 @@
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -39,6 +44,24 @@ class ChatViewModelTest {
     @Test
     fun testReceiveMessage_concurrentMessages() = runTest {
         val messagesToSend = (1..100).map { Message.MyMessage("Message $it") }
-
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val scope = CoroutineScope(Job() + testDispatcher)
+        val jobs = mutableListOf<Job>()
+        Dispatchers.setMain(testDispatcher)
+        try {
+            messagesToSend.forEach { message ->
+                val job = scope.launch {
+                    viewModel.sendMyMessage(message)
+                }
+                jobs.add(job)
+            }
+            jobs.joinAll()
+        } finally {
+            Dispatchers.resetMain()
+        }
+        assert(viewModel.messages.value.messages.size == 100)
+        viewModel.messages.value.messages.zip(messagesToSend).forEach { (messageInList, messageOriginal) ->
+            assert(messageOriginal.text == (messageInList as Message.MyMessage).text)
+        }
     }
 }
